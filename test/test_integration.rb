2c1b274d51496f325af5423d882e6fb2291f94fa
averygoodplanet <averygoodplanet@gmail.com>
2014-01-23 13:30:19 -0600

    pass # of cli args

--- test/test_integration.rb
+++ test/test_integration.rb
@@ -1,36 +1,11 @@
 require_relative 'helper'
 
 class TestIntegrationTests < MiniTest::Unit::TestCase
-  def test1_command_line_arguments_stored
-    command = `./recipe_tracker create 'My New Recipe`
-    assert_equal "create", ARGV[0]
-    assert_equal "'My New Recipe'", ARGV[1]
-  end
 
-  def test2_error_if_improper_number_of_command_line_arguments
+  def test1_error_if_more_than_two_command_line_arguments
     command = "./recipe_tracker one two three"
-    assert_command_output "Incorrect number of arguments, type 'help' for help.",
+    assert_command_output "Incorrect number of arguments (more than 2), type 'help' for help.",
     command
-  end
-
-####### Check to see if tests below need to be changed,
-#### *see class example of options parser.
-  def test_create_calls_its_method
-  end
-
-  def test_find_calls_its_method
-  end
-
-  def page_calls_its_method
-  end
-
-  def delete_calls_its_method
-  end
-
-  def query_calls_its_method
-  end
-
-  def help_calls_its_method
   end
 
   # def test_valid_purchase_information_gets_printed
