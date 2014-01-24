0ea78b13483fb7ea438b5e1ff5bba251d1d3bbe7
averygoodplanet <averygoodplanet@gmail.com>
2014-01-24 14:13:51 -0600

    create --> Recipe.create

--- models/recipe.rb
+++ models/recipe.rb
@@ -1,3 +1,8 @@
 class Recipe
-
+  def self.create(name, options)
+    options[:test_output] ? which_database = "test" : which_database = "production"
+    database = SQLite3::Database.new("db/recipe_tracker_#{which_database}.sqlite3")
+    sql_statement = "INSERT INTO recipes VALUES ('#{name}', '#{options[:ingredients]}', '#{options[:directions]}', #{options[:time]}, '#{options[:meal]}', #{options[:serves]}, #{options[:calories]})"
+    database.execute(sql_statement)
+  end
 end
