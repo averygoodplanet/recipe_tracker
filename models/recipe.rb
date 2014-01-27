5bf7395d9c8df7918d8e3e770ca16ed141362965
averygoodplanet <averygoodplanet@gmail.com>
2014-01-27 15:12:55 -0600

    connect_database_and_execute_sql_statement

--- models/recipe.rb
+++ models/recipe.rb
@@ -1,9 +1,14 @@
 class Recipe
-  def self.create(name, options)
+
+  def self.connect_database_and_execute_sql_statement(options, sql_statement)
     options[:test_output] ? which_database = "test" : which_database = "production"
     database = SQLite3::Database.new("db/recipe_tracker_#{which_database}.sqlite3")
-    sql_statement = "INSERT INTO recipes VALUES ('#{name}', '#{options[:ingredients]}', '#{options[:directions]}', #{options[:time]}, '#{options[:meal]}', #{options[:serves]}, #{options[:calories]})"
     database.execute(sql_statement)
+  end
+
+  def self.create(name, options)
+    sql_statement = "INSERT INTO recipes VALUES ('#{name}', '#{options[:ingredients]}', '#{options[:directions]}', #{options[:time]}, '#{options[:meal]}', #{options[:serves]}, #{options[:calories]})"
+    connect_database_and_execute_sql_statement(options, sql_statement)
   end
 
   def self.edit(options)
