c2725a70b0ae2243c42f123183ad15821da64ecc
averygoodplanet <averygoodplanet@gmail.com>
2014-01-28 09:44:01 -0600

    self.hash_to_array_of_strings

--- models/recipe.rb
+++ models/recipe.rb
@@ -12,11 +12,23 @@ class Recipe
   end
 
   def self.edit(name, options)
-    puts "In EDIT function, with old name #{name} and new name #{options[:recipe_name]}."
-    puts options
-#     sql_statement = "UPDATE recipes SET "
+    old_name = name
+    new_name = options[:recipe_name]
+    puts "In EDIT function, with old name #{old_name} and new name #{new_name}."
+    puts self.hash_to_array_of_strings(options)
+#     sql_statement = " "
 #     UPDATE table_name
 # SET column1=value1,column2=value2,...
 # WHERE some_column=some_value;
+  end
+
+  def self.hash_to_array_of_strings(hash)
+    array = hash.to_a
+    array_of_strings = []
+    for i in 0..(array.length-1)
+      assignment_string = array[i][0].to_s + "='" + array[i][1].to_s + "'"
+      array_of_strings.push(assignment_string)
+    end
+    array_of_strings
   end
 end
