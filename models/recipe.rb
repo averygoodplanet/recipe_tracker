fe1411e6d8a295fe52d40983085cc8a1e4b62921
averygoodplanet <averygoodplanet@gmail.com>
2014-01-28 12:16:57 -0600

    update method 'self.edit' passes test6

--- models/recipe.rb
+++ models/recipe.rb
@@ -1,23 +1,27 @@
 class Recipe
 
-  def self.connect_database_and_execute_sql_statement(options, sql_statement)
-    options[:test_output] ? which_database = "test" : which_database = "production"
+  def self.execute_sql(sql_statement, which_database = false)
+    which_database == true ? which_database = "test" : which_database = "production"
     database = SQLite3::Database.new("db/recipe_tracker_#{which_database}.sqlite3")
     database.execute(sql_statement)
   end
 
   def self.create(name, options)
     sql_statement = "INSERT INTO recipes VALUES ('#{name}', '#{options[:ingredients]}', '#{options[:directions]}', #{options[:time]}, '#{options[:meal]}', #{options[:serves]}, #{options[:calories]})"
-    connect_database_and_execute_sql_statement(options, sql_statement)
+    which_database = options.include?(:test_output)
+    execute_sql(sql_statement, which_database)
   end
 
   def self.edit(name, options)
     old_name = "'#{name}'"
     new_name = options[:recipe_name]
-    puts "In EDIT function, with old name #{old_name} and new name #{new_name}."
+    which_database = options.include?(:test_output)
+    # Need to remove test_output from options hash so that it isn't placed in
+    # the sql_statement.
+    options.delete(:test_output)
     serialization = self.serialize_hash(options)
     sql_statement = "UPDATE recipes SET #{serialization} WHERE recipe_name=#{old_name}"
-    puts sql_statement
+    execute_sql(sql_statement, which_database)
   end
 
   def self.serialize_hash(hash)
